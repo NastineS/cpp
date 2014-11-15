6b618dd48a2006fefdf2c1e6de6503e72da17277
Oleg Shcherbakov <shcherbakov.o@gmail.com>
2014-11-15 18:02:00 +0300

    isLucky

--- FirstProject/FirstProject/main.cpp
+++ FirstProject/FirstProject/main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
+	int n;
+	bool isLucky = false;;
+
+	cin >> n;
+
+	int a = 0, b = 0;
+	a = (n % 100) / 10 + n % 10;
+	n = n / 100;
+	b = (n % 100) / 10 + n % 10;;
+
+	isLucky = a == b;
+
+
+
+	cout << isLucky;
+
 	return 0;
 }
